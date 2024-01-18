e05e2aa3379d4091ab3d420a11b343fa1c7b31cd
Nanxiang Liu <nanxiangliu@microsoft.com>
2024-01-18 15:01:36 +0800

    fix syntax changelog

--- tools/PublishModules.ps1
+++ tools/PublishModules.ps1
@@ -56,7 +56,10 @@ param(
     [string]$RepositoryLocation,
 
     [Parameter(Mandatory = $false, Position = 5)]
-    [string]$NugetExe
+    [string]$NugetExe,
+
+    [Parameter(Mandatory = $false, Position = 5)]
+    [boolean]$GenerateDocumentationFile = $false
 )
 
 Import-Module "$PSScriptRoot\PublishModules.psm1"
@@ -119,6 +122,8 @@ $Errors = $null
 try {
     $modules = Get-AllModules -BuildConfig $BuildConfig -Scope $Scope -PublishLocal:$PublishLocal -IsNetCore:$IsNetCore
     Add-AllModules -ModulePaths $modules -TempRepo $tempRepoName -TempRepoPath $tempRepoPath -NugetExe $NugetExe
+    .("$PSScriptRoot/CleanupBuild.ps1 -BuildConfig $BuildConfig -GenerateDocumentationFile $GenerateDocumentationFile")
+    .("$PSScriptRoot/NewOutputTypeIndex.ps1 -OutputFile $packageFolder/outputtypes.json -BuildConfig $BuildConfig")
     Publish-AllModules -ModulePaths $modules -ApiKey $apiKey -TempRepoPath $tempRepoPath -RepoLocation $repositoryLocation -NugetExe $NugetExe -PublishLocal:$PublishLocal
 } catch {
     $Errors = $_
